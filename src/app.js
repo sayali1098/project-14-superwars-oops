285cecde3fb63ccab944070446748ad40d0c4fe2
sayali1098 <sauamritkar@gmail.com>
2020-06-22 14:09:32 +0530

    FSD0620B102

--- src/app.js
+++ src/app.js
@@ -1,86 +1,96 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    // "Popcorn",
-    // "Gemwoman",
-    // "Bolt",
-    // "Antwoman",
-    // "Mask",
-    // "Tiger",
-    // "Captain",
-    // "Catwoman",
-    // "Fish",
-    // "Hulk",
-    // "Ninja",
-    // "Black Cat",
-    // "Volverine",
-    // "Thor",
-    // "Slayer",
-    // "Vader",
-    // "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  // "Popcorn",
+  // "Gemwoman",
+  // "Bolt",
+  // "Antwoman",
+  // "Mask",
+  // "Tiger",
+  // "Captain",
+  // "Catwoman",
+  // "Fish",
+  // "Hulk",
+  // "Ninja",
+  // "Black Cat",
+  // "Volverine",
+  // "Thor",
+  // "Slayer",
+  // "Vader",
+  // "Slingo"
 ];
 
 // Player Class
 class Player {
-    constructor(id, name, type) {
-        // Create member variables and assign values
-        // Type your code
+  constructor(id, name, type) {
+    // Create member variables and assign values
+    // Type your code
+    this.id = id;
+    this.name = name;
+    this.image = "images/super-" + (id + 1) + ".png";
+    this.strength = this.getRandomStrength();
+    this.type = type;
+  }
 
-    }
+  // getting random strength
+  getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+  };
 
-    // getting random strength
-    getRandomStrength = () => {
-        return Math.ceil(Math.random() * 100);
-    }
+  // Create a player for displaying
+  view = () => {
+    // Accumulate HTML template
+    // Type your code here
 
-    // Create a player for displaying
-    view = () => {
-        // Accumulate HTML template
-        // Type your code here
-
-    
-        return player;
-    }
+    //First I have created an element div. Inside div I'm going to put all these HTML. Finally I'm returning element div. That's it
+    var element = document.createElement("div");
+    let player = `<div class="player" data-id="${this.id}">
+                    <img src="${this.image}" alt="">
+                    <div class="name">${this.name}</div>
+                    <div class="strength">${this.strength}</div>
+                    </div>`;
+    element.innerHTML = player;
+    return element.lastChild;
+  };
 }
 
 // Superwar Class
 class Superwar {
-    constructor(players) {
-    // Create a field players 
+  constructor(players) {
+    // Create a field players
     // Use Map method to loop through players argument and create new players
     // Type your code here
+    this.players = players.map((player, index) => {
+      let type = index % 2 == 0 ? "hero" : "villain";
+      return new Player(index, player, type);
+    });
+  }
 
-    }
+  // Display players in HTML
+  viewPlayers = () => {
+    let team = document.getElementById("heroes");
+    team.innerHTML = "";
+    let fragment = this.buildPlayers("hero");
+    team.append(fragment);
 
-    // Display players in HTML
-    viewPlayers = () => {
-        let team = document.getElementById('heroes');
-        team.innerHTML = '';
-        let fragment =
-            this.buildPlayers('hero');
-        team.append(fragment);
+    team = document.getElementById("villains");
+    team.innerHTML = "";
+    fragment = this.buildPlayers("villain");
+    team.append(fragment);
+  };
 
-        team = document.getElementById('villains');
-        team.innerHTML = '';
-        fragment =
-            this.buildPlayers('villain');
-        team.append(fragment);
-    }
-
-    // Build players fragment 
-    buildPlayers = (type) => {
-        let fragment = document.createDocumentFragment();
-        this.players
-            .filter(player => player.type == type)
-            .forEach(player => fragment.append(player.view()));
-        return fragment;
-    }
-
+  // Build players fragment
+  buildPlayers = (type) => {
+    let fragment = document.createDocumentFragment();
+    this.players
+      .filter((player) => player.type == type)
+      .forEach((player) => fragment.append(player.view()));
+    return fragment;
+  };
 }
-
 
 window.onload = () => {
-    const superwar = new Superwar(PLAYERS);
-    superwar.viewPlayers();
-}
+  const superwar = new Superwar(PLAYERS);
+  superwar.viewPlayers();
+};
